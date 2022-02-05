0075f48dc8e0891c31e2440589c76550c2200a0d
xryshirfxn <95993137+xryshirfxn@users.noreply.github.com>
2022-02-06 07:26:30 +0800

    feat: create boot.asm

--- src/boot.asm
+++ src/boot.asm
@@ -1,1 +1,15 @@
-;; ...
+%include "common.asm"
+
+startup_arch:
+    ;; Load PMode and GDT
+    cli
+    lgdt [gdtr]
+    lidt [idtr]
+
+    ;; Set PMode bit to `CR0`
+    MOV EAX, CR0
+    OR EAX, 1
+    MOV CR0, EAX
+
+    ;; Far jump to load CS with 32-bit segment
+    ;; ...
